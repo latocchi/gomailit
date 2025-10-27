abeedcb7be1e7ad4eeb8cab2d90d1b55918d63fb
latocchi <jaycybanaga@gmail.com>
2025-10-27 17:19:28 +0800

    removing todo

--- cmd/root.go
+++ cmd/root.go
@@ -9,8 +9,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// TODO: Send email with attachments
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gomailit",
