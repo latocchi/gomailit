6a0658a8834995bfe247d92c8850e1505c1af3ee
latocchi <jaycybanaga@gmail.com>
2025-10-28 21:44:25 +0800

    fixed description

--- cmd/root.go
+++ cmd/root.go
@@ -13,12 +13,6 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "gomailit",
 	Short: "A command-line tool that allows user to send email via terminal",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
